f7ea8ff4ef719c060aed0aaeba9d37a33449ffab
Sahal <sahal@bairuhatech.com>
2023-10-03 11:45:00 +0400

    upadated first screen

--- src/App/homeScreen/index.tsx
+++ src/App/homeScreen/index.tsx
@@ -5,6 +5,7 @@ import "./styles.scss";
 import Car from "../../asset/image/image 18.png";
 import CarSeat from "../../asset/image/image 17.png";
 import Logo from "../../asset/image/path4.png";
+import DownloadImg from "../../asset/image/arrow-down-circle-outline.png";
 import { Carousel } from "antd";
 function HomeScreen() {
   return (
@@ -23,9 +24,35 @@ function HomeScreen() {
         </div>
         <div className="homeScreen-Box3">
           <div className="homeScreen-Box4">
-            <img className="homeScrn-Logo" src={Logo} alt="" />
+            <div className="homeScreen-Box5">
+              <div className="homeScreen-Box6">
+                <img className="img-fluid" src={Logo} alt="" />
+              </div>
+              <div>
+                <br />
+                <div className="homeScreen-Txt1">Redefining Identity</div>
+                <br />
+                <div className="homeScreen-Txt2">
+                  Welcome to Fabro, where you'll find stylish car seat covers
+                  that enhance your car's appearance, provide comfort, and
+                  protect your seat. We offer a wide selection to match your
+                  style.
+                </div>
+              </div>
+              <br />
+              <div className="homeScreen-Box7">
+                <div>
+                  <button className="homeScreen-Box8">Contact</button>
+                </div>
+                <div>
+                  <button className="homeScreen-Box9">
+                    Download <img src={DownloadImg} />
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="homeScreen-Txt1">Redefining Identity</div>
+          <div className="homeScreen-Box10"></div>
         </div>
       </div>
       <WebsiteFooter />
