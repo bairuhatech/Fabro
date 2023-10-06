15c6e800c20b276525d7b69f38ffa04e59c551b8
re220519 <kreshma.k54@gmail.com>
2023-10-06 09:24:23 +0400

    updated websiteHeader fabro

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomeScreen from "./App/homeScreen";
 import { ConfigProvider } from "antd";
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 function App() {
   return (
--- src/App/homeScreen/index.tsx
+++ src/App/homeScreen/index.tsx
@@ -1,3 +1,4 @@
+// HomeScreen.js
 import React from "react";
 import WebsiteFooter from "../../component/websiteFooter";
 import WebsiteHeader from "../../component/websiteHeader";
@@ -6,27 +7,37 @@ import Car from "../../asset/image/image 18.png";
 import CarSeat from "../../asset/image/image 17.png";
 import Logo from "../../asset/image/path4.png";
 import { Carousel } from "antd";
+
 function HomeScreen() {
   return (
     <div className="homeScreen-Box1">
-      <WebsiteHeader />
+      
       <div className="homeScreen-Box2">
-        <div>
+     <div>
+     
+        
+          
           <Carousel autoplay dotPosition="right" effect="fade">
-            <div>
+          
+            <div className="carousal">
+            
               <img className="homeScreen-carousalImg" src={Car} alt="" />
             </div>
-            <div>
+            <div className="carousal">
               <img className="homeScreen-carousalImg" src={CarSeat} alt="" />
             </div>
           </Carousel>
-        </div>
+        
         <div className="homeScreen-Box3">
+        <WebsiteHeader/>
           <div className="homeScreen-Box4">
+          
             <img className="homeScrn-Logo" src={Logo} alt="" />
           </div>
           <div className="homeScreen-Txt1">Redefining Identity</div>
         </div>
+        
+      </div>
       </div>
       <WebsiteFooter />
     </div>
