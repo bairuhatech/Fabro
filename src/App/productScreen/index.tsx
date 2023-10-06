606c275b03ba103986d05b81bb63ba9cdd4f94de
Anas@bairuhaTech <anas@bairuhatech.com>
2023-10-06 11:55:33 +0530

    updated

--- src/App/productScreen/index.tsx
+++ src/App/productScreen/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Collections from "./component/block1";
+import ProductSearch from "./component/block2";
 
 function ProductScreen() {
-  return <div className="productScreen-Box1">Product-Screen</div>;
+  return (
+    <>
+      <div>
+        <Collections />
+      </div>
+      <div>
+        <ProductSearch />
+      </div>
+    </>
+  );
 }
 
 export default ProductScreen;
