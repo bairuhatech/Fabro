efab06cc6c046d3b441df1327dc678a62c5427f6
Sahal <sahal@bairuhatech.com>
2023-10-06 10:31:53 +0400

    uuu

--- src/App/productScreen/component/block1.tsx
+++ src/App/productScreen/component/block1.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles.scss";
-import { Row, Col } from "react-bootstrap";
+// import { Row, Col } from "react-bootstrap";
 function Collections() {
   return (
     <>
