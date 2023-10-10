f2bceeb54393851f3938a620b935949cad58dfd1
sajeer-pa <sajeerpa1607@gmail.com>
2023-10-10 16:30:08 -0700

    product screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import Home from "./App/index";
+import ViewPageScreen from "./App/viewPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/viewPageScreen" element={<ViewPageScreen />} />
       </Routes>
     </>
   );
