70899183d80f4a4fca3e61c2491904c04dbd309c
sajeer-pa <sajeerpa1607@gmail.com>
2023-10-10 17:55:19 -0700

    error finish

--- src/asset/index.tsx
+++ src/asset/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import WebsiteHeader from "../component/websiteHeader";
-import HomeScreen from "./homeScreen";
-import ProductScreen from "./productScreen";
 import WebsiteFooter from "../component/websiteFooter";
-import ContactScreen from "./contactScreen";
-import ViewPageScreen from "./viewPage";
+import ViewPageScreen from "../App/viewPage";
+import ContactScreen from "../App/contactScreen";
+import HomeScreen from "../App/homeScreen";
+import ProductScreen from "../App/productScreen";
 
 const Home = () => {
   return (
